41a986ae054851f4e29b3129aa8be7b55acf03e3
Tristan Barrow <tristanbarrow@205-185-99-107.directcom.com>
2018-06-25 23:14:43 -0600

    setup Hello Overboard

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello OverBoard!');
+});
+
+app.listen(PORT, console.log('Over Board is Listening on Port: ' + PORT));
