8b381bcd5d41054605ba9c0c53e96bf97b74b2be
Tristan Barrow <tristanbarrow@205-185-99-107.directcom.com>
2018-06-30 23:16:20 -0600

    fixed double json parse

--- private/db-access/boards.js
+++ private/db-access/boards.js
@@ -3,7 +3,7 @@ const pool = require('./initdb');
 const getBoards = (callback) => {
     const query = 'SELECT name FROM boards';
     pool.query(query, null, (err, result) => {
-        callback(err, JSON.stringify(result.rows));
+        callback(err, result.rows);
     });
 }
 
