93d1355e2c0b52bc131bf0880d9a832cbf8b89a3
Tristan Barrow <tristanbarrow@205-185-99-107.directcom.com>
2018-07-14 13:08:27 -0600

    fixed sql styling

--- private/db-access/boards.js
+++ private/db-access/boards.js
@@ -3,6 +3,7 @@ const pool = require('./initdb');
 const getBoards = (callback) => {
     const query = 'SELECT name FROM boards';
     pool.query(query, null, (err, result) => {
+        
         console.log(result.rows);
         callback(err, result.rows);
     });
@@ -10,7 +11,7 @@ const getBoards = (callback) => {
 
 const getTricks = (board, callback) => {
     console.log(board);
-    const query = 'select * from tricks where board = (select id from boards where name = $1);';
+    const query = 'SELECT * FROM tricks WHERE board = (SELECT id FROM boards WHERE name = $1);';
     pool.query(query, [board], (err, result) => {
         callback(err, result.rows);
     });
