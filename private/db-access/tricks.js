7220c2ae3b30b4236a9ae7821938a42abe6a5f64
Tristan Barrow <tristanbarrow@205-185-99-107.directcom.com>
2018-07-14 13:08:05 -0600

    setup tricks routes

--- private/db-access/tricks.js
+++ private/db-access/tricks.js
@@ -1,7 +1,10 @@
 const pool = require('./initdb.js');
 
 const getTricks = (username, callback) => {
-    
+    const query = 'SELECT t.name AS trick, p.proficiency_title AS prof_title, p.proficiency_notes AS prof_notes, ut.notes AS notes FROM users_tricks ut INNER JOIN users u ON u.id = ut.username INNER JOIN tricks t ON t.id = ut.trick INNER JOIN proficiencies p ON ut.proficiency = p.id WHERE u.username = $1';
+    pool.query(query, [username], (err, result) => {
+        callback(err, result.rows);
+    });
 }
 
 // const trick = { 
@@ -12,11 +15,17 @@ const getTricks = (username, callback) => {
 // }
 
 const addTrick = (trick, callback) => {
-
+    const query = '';
+    pool.query(query, [username], (err, result) => {
+        callback(result);
+    });
 }
 
 const deleteTrick = (trick, callback) => {
-
+    const query = '';
+    pool.query(query, [username], (err, result) => {
+        callback(result);
+    });
 }
 
 module.exports = {
