45c1858a283665d7e431dd9aea57d65e1fad1ad5
Tristan Barrow <tristanbarrow@205-185-99-107.directcom.com>
2018-07-15 03:36:03 -0600

    trying status 300

--- private/endpoints/pages.js
+++ private/endpoints/pages.js
@@ -1,15 +1,21 @@
 const p = require('path');
 
+
 const redirect = (req, res) => {
     if (req.session.user) {
+        res.status(301);
+        //res.render("<script> window.location = '/home'; </script>");
         res.redirect('/home');
     } else {
+        res.status(301);
+        //res.render("<script> window.location = '/login'; </script>");
         res.redirect('/login');
     }
 }
 
 const login = (req, res) => {
     if (req.session.user) {
+        res.status(301);
         res.redirect('/home');
     }
     res.sendFile(p.join(__dirname, '..', '..', 'pages', 'login.html'));
@@ -17,6 +23,7 @@ const login = (req, res) => {
 
 const createAccount = (req, res) => {
     if (req.session.user) {
+        res.status(301);
         res.redirect('/home');
     }
     res.sendFile(p.join(__dirname, '..', '..', 'pages', 'create-account.html'));
@@ -24,6 +31,7 @@ const createAccount = (req, res) => {
 
 const home = (req, res) => {
     if (!req.session.user) {
+        res.status(301);
         res.redirect('/login');
     }
     res.sendFile(p.join(__dirname, '..', '..', 'pages', 'home.html'));
