b0567e88f9ba74f5bf98d47c34069ddb47fa5801
Tristan Barrow <tristanbarrow@205-185-99-107.directcom.com>
2018-07-15 03:39:35 -0600

    trying client redirect

--- private/endpoints/pages.js
+++ private/endpoints/pages.js
@@ -3,38 +3,45 @@ const p = require('path');
 
 const redirect = (req, res) => {
     if (req.session.user) {
-        res.status(301);
-        //res.render("<script> window.location = '/home'; </script>");
-        res.redirect('/home');
+        //res.status(301);
+        res.render("<script> window.location = '/home'; </script>");
+        //res.redirect('/home');
     } else {
-        res.status(301);
-        //res.render("<script> window.location = '/login'; </script>");
-        res.redirect('/login');
+        //res.status(301);
+        res.render("<script> window.location = '/login'; </script>");
+        //res.redirect('/login');
     }
 }
 
 const login = (req, res) => {
     if (req.session.user) {
-        res.status(301);
-        res.redirect('/home');
+        //res.status(301);
+        //res.redirect('/home');
+        res.render("<script> window.location = '/home'; </script>");
+    } else {
+        res.sendFile(p.join(__dirname, '..', '..', 'pages', 'login.html'));
     }
-    res.sendFile(p.join(__dirname, '..', '..', 'pages', 'login.html'));
 }
 
 const createAccount = (req, res) => {
     if (req.session.user) {
-        res.status(301);
-        res.redirect('/home');
+        // res.status(301);
+        // res.redirect('/home');
+        res.render("<script> window.location = '/home'; </script>");
+    } else {
+        res.sendFile(p.join(__dirname, '..', '..', 'pages', 'create-account.html'));
     }
-    res.sendFile(p.join(__dirname, '..', '..', 'pages', 'create-account.html'));
 }
 
 const home = (req, res) => {
     if (!req.session.user) {
-        res.status(301);
-        res.redirect('/login');
+        // res.status(301);
+        // res.redirect('/login');
+        res.render("<script> window.location = '/home'; </script>");
+    } else {
+        res.sendFile(p.join(__dirname, '..', '..', 'pages', 'home.html'));
     }
-    res.sendFile(p.join(__dirname, '..', '..', 'pages', 'home.html'));
+    
 }
 
 module.exports = {
